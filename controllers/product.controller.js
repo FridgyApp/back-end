d0ea955d713eac44149f0a9d7071a09a16cd4af8
Bruno Aggierni <bruno.aggierni@gmail.com>
2021-07-15 12:01:30 +0100

    EndPoints Checked and working

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,29 +3,6 @@ const mongoose = require("mongoose");
 
 const ProductModel = require("../models/product.model");
 const CategoryModel = require("../models/category.model");
-// const ShoppingListController = require("./falseShopping");
-
-const createProduct = async (req, res = response) => {
-  req.body.category = mongoose.Types.ObjectId("60e587407435c4c3713e1483");
-  try {
-    const createOwnProduct = await ProductModel.create(req.body);
-    createOwnProduct.save();
-    const findCategoryId = await CategoryModel.findById(
-      "60e587407435c4c3713e1483"
-    );
-    findCategoryId.products.push(createOwnProduct);
-    await findCategoryId.save();
-
-    req.body.productId = createOwnProduct._id;
-    req.body.notes = "";
-    req.body.bought = false;
-
-    await ShoppingListController.addProductToList(req, res);
-    
-  } catch (error) {
-    console.log("Error", error);
-  }
-};
 
 const getProducts = async (req, res) => {
   try {
@@ -45,6 +22,32 @@ const getProducts = async (req, res) => {
     console.log("Error", error);
   }
 };
+
+const createProduct = async (req, res = response) => {
+  req.body.category = mongoose.Types.ObjectId("60e587407435c4c3713e1483");
+  try {
+    const createOwnProduct = await ProductModel.create(req.body);
+    createOwnProduct.save();
+    const findCategoryId = await CategoryModel.findById(
+      "60e587407435c4c3713e1483"
+    );
+    findCategoryId.products.push(createOwnProduct);
+    await findCategoryId.save();
+
+    req.body.productId = createOwnProduct._id;
+    req.body.notes = "";
+    
+
+    //falta asignarle su groupId
+
+    res.json(req.body)
+    
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
+
+
 
 module.exports = {
   createProduct,
--- routers/index.js
+++ routers/index.js
@@ -2,15 +2,14 @@ const router = require('express').Router()
 
 const routerProduct = require('./product.router')
 const GroupRouter = require('./group.router')
-const ShopListRouter = require('./shopList.router')
+const ShoppingListRouter = require('./shoppingList.router')
 const CategoryRouter = require('./category.router')
 const authRouter = require('./auth.router')
 
 router
-  // .use('/shoppingList',routerShopping)
   .use('/products', routerProduct)
   .use('/group',GroupRouter)
-  .use('/shopping', ShopListRouter)
+  .use('/shoppingList', ShoppingListRouter)
   .use('/category', CategoryRouter)
   .use('/auth', authRouter)
 
